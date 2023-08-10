98355c1e023ab1d1677d6ae4074a3ed7f078905d
Robert Gildein <robert.gildein@canonical.com>
2023-08-10 18:40:01 +0200

    remove aliases, manually create in func tests

--- tests/functional/test_snap_tempest.py
+++ tests/functional/test_snap_tempest.py
@@ -9,4 +9,11 @@ def test_tempest_help_string():
 def test_tempestconf_help():
     """Test python-tempestconf prints the help."""
     assert 0 == subprocess.run(["tempest.discover", "--help"]).returncode
+    # TODO: remove this after auto-aliasing is enabled
+    assert (
+        0
+        == subprocess.run(
+            ["sudo", "snap", "alias", "tempest.discover", "discover-tempest-config"]
+        ).returncode
+    )
     assert 0 == subprocess.run(["discover-tempest-config", "--help"]).returncode
