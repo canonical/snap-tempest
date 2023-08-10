5b3ea87c6fa511158f86a4677170708203c25c3b
Robert Gildein <robert.gildein@canonical.com>
2023-08-10 10:18:31 +0200

    Integrate tempestconf
    Integrate python-tempestconf and add simple functional tests for it.

--- tests/functional/test_snap_tempest.py
+++ tests/functional/test_snap_tempest.py
@@ -4,3 +4,9 @@ import subprocess
 def test_tempest_help_string():
     """Test tempest prints the help string."""
     assert 0 == subprocess.run(["tempest", "--help"]).returncode
+
+
+def test_tempestconf_help():
+    """Test python-tempestconf prints the help."""
+    assert 0 == subprocess.run(["tempest.discover", "--help"]).returncode
+    assert 0 == subprocess.run(["discover-tempest-config", "--help"]).returncode
