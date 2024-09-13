53086ef6a907e56b3254f2fb4fb969b04c0f2678
Samuel Allan <samuel.allan@canonical.com>
2024-09-13 14:41:22 +0930

    fix lint

--- tests/functional/test_snap_tempest.py
+++ tests/functional/test_snap_tempest.py
@@ -10,9 +10,7 @@ def test_tempest_help_string():
 def test_tempestconf_help():
     """Test python-tempestconf prints the help."""
     # aliases aren't created yet
-    subprocess.check_call(
-        ["sudo", "snap", "alias", "tempest.discover", "discover-tempest-config"]
-    )
+    subprocess.check_call(["sudo", "snap", "alias", "tempest.discover", "discover-tempest-config"])
     output1 = subprocess.check_call(["tempest.discover", "--help"])
     output2 = subprocess.check_output(["discover-tempest-config", "--help"])
     assert output1 == output2
