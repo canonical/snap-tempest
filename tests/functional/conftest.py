1efa73d8930865bab5bf3d16b1519e4197038fe0
Samuel Allan <samuel.allan@canonical.com>
2024-09-13 14:21:25 +0930

    Remove old files, refactor check workflow

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -6,8 +6,8 @@ import pytest
 
 @pytest.fixture(scope="session", autouse=True)
 def install_tempest_snap():
-    snap = os.environ["TEST_SNAP"]
-    process = subprocess.run(["sudo", "snap", "install", "--dangerous", snap])
-    assert 0 == process.returncode
+    subprocess.check_call("sudo snap install --dangerous tempest_*.snap", shell=True)
+
     yield
+
     subprocess.run(["sudo", "snap", "remove", "--purge", "tempest"])
